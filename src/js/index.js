5a0835939f9313d7b19136ab7c44e1a2a77a9767
[dabinderudhan] <[dabinder.udhan@gmail.com]>
2022-03-15 17:51:58 +0530

    changes in js file

--- src/js/index.js
+++ src/js/index.js
@@ -11,73 +11,74 @@ const peopleError = document.querySelector(".input-data--error");
 const btnReset = document.querySelector(".output-reset");
 
 let billAmount = 0;
-
 let customTip = 0;
-let tipPercent;
-
-let noOfPeople;
-
+let tipPercent = 0;
+let noOfPeople = (peopleValue.value = 1);
 let tipPerPerson = 0;
 let billPerPerson = 0;
 
+// this function will show error
 function showError(errorMsg, errorBox) {
   errorMsg.style.opacity = 1;
   errorBox.classList.add("displayError");
 }
 
+// this function will remove error
 function removeError(errorMsg, errorBox) {
   errorMsg.style.opacity = 0;
   errorBox.classList.remove("displayError");
 }
 
-validatePeopleValue = () => {
-  if (peopleValue.value === "" || peopleValue.value === 0) {
-    showError(peopleError, peopleValue);
-  } else noOfPeople = peopleValue.value;
-  return noOfPeople;
-};
-
+// this function will calculate tip per person and will display Tip amount per person.
 function calcTipPerPerson() {
   billAmount = billValue.value;
 
-  if (validatePeopleValue()) {
-    removeError(peopleError, peopleValue);
-    tipPerPerson = (billAmount * tipPercent) / noOfPeople;
-    displayTipAmount.innerText = tipPerPerson.toFixed(2);
-    btnReset.classList.add("active");
-  }
+  tipPerPerson = (billAmount * tipPercent) / noOfPeople;
+  displayTipAmount.innerText = tipPerPerson.toFixed(2);
+  btnReset.classList.add("active");
 }
 
+// this function will calculate bill per person and will display bill amount per person.
 function calcTotalBillPerPerson() {
   billAmount = billValue.value;
 
-  if (validatePeopleValue()) {
-    removeError(peopleError, peopleValue);
-    billPerPerson = billAmount / noOfPeople + tipPerPerson;
-    displayTotalAmount.innerText = billPerPerson.toFixed(2);
-    btnReset.classList.add("active");
-  }
+  billPerPerson = billAmount / noOfPeople + tipPerPerson;
+  displayTotalAmount.innerText = billPerPerson.toFixed(2);
+  btnReset.classList.add("active");
 }
 
+// this function will get the value of the custom button and will calculate the tip and bill per person
 function customTipInput() {
-  customTipValue.addEventListener("change", function () {
+  customTipValue.addEventListener("input", function () {
     customTip = customTipValue.value;
+    console.log(customTip);
+
     tipPercent = parseFloat(customTip / 100);
+    console.log(tipPercent);
 
     calcTipPerPerson();
     calcTotalBillPerPerson();
-
-    // return customTip;
   });
+  return tipPercent;
 }
 
-peopleValue.addEventListener("change", function () {
-  calcTipPerPerson();
-  calcTotalBillPerPerson();
+peopleValue.addEventListener("input", function () {
+  noOfPeople = peopleValue.value;
+
+  if (noOfPeople === "" || noOfPeople === "0") {
+    showError(peopleError, peopleValue);
+    tipPerPerson = 0;
+    billPerPerson = 0;
+    displayTipAmount.innerText = tipPerPerson.toFixed(2);
+    displayTotalAmount.innerText = billPerPerson.toFixed(2);
+  } else {
+    calcTipPerPerson();
+    calcTotalBillPerPerson();
+    removeError(peopleError, peopleValue);
+  }
 });
 
 tipValue.forEach((btn) => {
-  // customTip = customTipValue.value;
   btn.addEventListener("click", function (e) {
     const id = e.target.dataset.id;
 
@@ -86,6 +87,8 @@ tipValue.forEach((btn) => {
       customTipValue.value = "";
     } else {
       customTipInput();
+      console.log(customTip);
+      console.log(tipPercent);
     }
 
     calcTipPerPerson();
